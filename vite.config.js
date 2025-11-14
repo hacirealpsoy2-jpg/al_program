7799fddc675781855c58df297563e080417dd5d6
FizikCozumEkibi <hacirealpsoy2@gmail.com>
2025-11-15 00:38:24 +0300

    Format vite.config.js for consistency

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',           // Render container dışına açıyor
-    port: process.env.PORT || 5173, // Render portunu kullan
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173
   }
 })
